71ce29331e432b9714c3ec8ebada3b21f5fd4924
Emre Kayan <emrekayan@gmail.com>
2015-08-16 20:37:13 +0300

    Add Instagram authentication

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -1,11 +1,25 @@
 var express = require('express');
+var Instagram = require('../lib/sdk/instagram');
+var config = require('../config');
+
 var router = express.Router();
 
 router.get('/instagram', function(req, res, next) {
   if (req.query.hasOwnProperty('code')) {
     // get instagram access token using req.query.code
+    var instagram = new Instagram(config.instagram);
+    instagram.getAccessToken(req.query.code, function (err) {
+      if (err) {
+        res.send(err.message);
+        return;
+      }
+      res.send('user id: '+instagram.userID+', received access token: '+instagram.accessToken);
+      return;
+    });
+  } else {
+    // todo: show error
+    res.send('no code found');
   }
-  res.end();
 });
 
 router.get('/dropbox', function(req, res, next) {
