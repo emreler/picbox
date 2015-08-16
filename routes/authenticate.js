cb7acff076ebb7f32e1e037a7eadf862dcf21bdb
Emre Kayan <emrekayan@gmail.com>
2015-08-16 13:41:54 +0300

    Add basic landing page with OAuth redirections

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -5,12 +5,14 @@ router.get('/instagram', function(req, res, next) {
   if (req.query.hasOwnProperty('code')) {
     // get instagram access token using req.query.code
   }
+  res.end();
 });
 
 router.get('/dropbox', function(req, res, next) {
   if (req.query.hasOwnProperty('code')) {
     // get dropbox access token using req.query.code
   }
+  res.end();
 });
 
 module.exports = router;
