ca0f9f787d16ce67ab7d2e5a662acdb46affb354
Emre Kayan <emrekayan@gmail.com>
2015-08-21 00:39:35 +0300

    Redirect authenticated users to homepage

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/',
+function (req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/home');
+  }
+  next();
+},
+function(req, res, next) {
   res.render('index');
 });
 
