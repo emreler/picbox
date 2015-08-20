8fad85c83cafa650b3c3aea53f9869552c4405c1
Emre Kayan <emrekayan@gmail.com>
2015-08-21 01:50:26 +0300

    Add home route.

--- routes/accounts.js
+++ routes/accounts.js
@@ -2,12 +2,20 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
+var ensureAuthenticated = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect('/');
+  }
+};
+
 router.get('/signin', function (req, res, next) {
   // todo: redirect to homepage
   res.end();
 });
 
-router.post('/signin', passport.authenticate('local', {successRedirect: '/', failureRedirect: '/login'}));
+router.post('/signin', passport.authenticate('local', {successRedirect: '/home', failureRedirect: '/'}));
 
 router.get('/signup', function (req, res, next) {
   // todo: redirect to homepage
@@ -16,6 +24,10 @@ router.get('/signup', function (req, res, next) {
 
 router.post('/signup', function (req, res, next) {
   res.end();
+});
+
+router.get('/home', ensureAuthenticated, function (req, res, next) {
+  res.render('home');
 });
 
 module.exports = router;
