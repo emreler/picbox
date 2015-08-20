a7914e4d01e400076a5e388f68955050f38cba57
Emre Kayan <emrekayan@gmail.com>
2015-08-21 00:31:09 +0300

    Save access token and id of the user after auth is complete

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -1,24 +1,26 @@
 var express = require('express');
 var Instagram = require('../app/sdk/instagram');
 var Dropbox = require('../app/sdk/dropbox');
+var Storage = require('../app/storage');
 var config = require('../config');
 
 var router = express.Router();
+var storage = new Storage(config.mysql);
 
 router.get('/instagram', function(req, res, next) {
   if (req.query.hasOwnProperty('code')) {
     // get instagram access token using req.query.code
     var instagram = new Instagram(config.instagram);
-    instagram.getAccessToken(req.query.code, function (err) {
-      if (err) {
-        res.send(err.message);
-        return;
+    instagram.getAccessToken(req.query.code, function (err, accessToken, user) {
+      if (err || !accessToken || !user) {
+        return res.send(err.message);
       }
-      res.send('user id: '+instagram.userID+', received access token: '+instagram.accessToken);
-      return;
+      storage.saveInstagramInfo(req.user.email, {accessToken: accessToken, userID: user.id}, function (updated) {
+        return res.send('saved');
+      });
     });
   } else {
-    next(new Error('No code found'));
+    return next(new Error('No code found'));
   }
 });
 
@@ -26,13 +28,14 @@ router.get('/dropbox', function(req, res, next) {
   if (req.query.hasOwnProperty('code')) {
     // get dropbox access token using req.query.code
     var dropbox = new Dropbox(config.dropbox);
-    dropbox.getAccessToken(req.query.code, function (err) {
+    dropbox.getAccessToken(req.query.code, function (err, accessToken, userID) {
       if (err) {
         res.send(err.message);
         return;
       }
-      res.send('user id: '+dropbox.userID+', received access token: '+dropbox.accessToken);
-      return;
+      storage.saveDropboxInfo(req.user.email, {accessToken: accessToken, userID: userID}, function (updated) {
+        return res.send('saved');
+      });
     });
   } else {
   next(new Error('No code found'));
