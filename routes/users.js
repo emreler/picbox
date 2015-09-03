f23726da573744821d8933f7cf473b16aadf1d73
Emre Kayan <emrekayan@gmail.com>
2015-09-04 01:13:09 +0300

    Improve home page after sign in

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var moment = require('moment');
 
 module.exports = function (app) {
   var ensureAuthenticated = function (req, res, next) {
@@ -13,13 +14,18 @@ module.exports = function (app) {
 
     var linkedIgm = !!(req.user.instagram_id && req.user.instagram_token);
     var linkedDbx = !!(req.user.dropbox_id && req.user.dropbox_token);
+    var lastSync = req.user.last_sync ? moment.unix(req.user.last_sync).fromNow() : null;
+    var username = req.user.username || req.user.email.substring(0, req.user.email.indexOf('@'));
 
-
+    console.log(lastSync);
     app.render('home', {
+      username: username,
       email: req.user.email,
       linkedIgm: linkedIgm,
-      linkedDbx: linkedDbx
+      linkedDbx: linkedDbx,
+      lastSync: lastSync
     }, function (err, content) {
+      if (err) console.error(err);
       res.render('layouts/main', {content: content});
     });
   });
