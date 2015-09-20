5c52e49c418569596698668d4fb57c3e9d57d649
Emre Kayan <emrekayan@gmail.com>
2015-09-20 21:46:57 +0300

    Add New Relic

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var flash = require('connect-flash');
-require('newrelic');
 
 var app = express();
 
