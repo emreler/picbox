f679aef054e1bb3debee94a44121c090b3318d1f
Emre Kayan <emrekayan@gmail.com>
2015-10-13 01:49:57 +0300

    Fix initial phase on counter

--- app/socket/index.js
+++ app/socket/index.js
@@ -6,11 +6,12 @@ var storage = new Storage(config.mysql, config.redis);
 module.exports = function (server) {
   var io = require('socket.io').listen(server);
   io.on('connection', function (socket) {
-    setInterval(function () {
+    var emit = function () {
       storage.getTotalSavedCount().then(function (totalSavedCount) {
         io.emit('up', totalSavedCount);
       });
-    }, 60000);
+    }();
+    setInterval(emit, 60000);
     socket.on('disconnect', function () {
 
     });
